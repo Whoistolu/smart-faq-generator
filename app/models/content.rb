b3aa1afb0db768e206966cce65721c0503ec4e6d
Whoistolu <digittolu25@gmail.com>
2025-08-12 17:12:33 +0100

    Add validation and associations

--- app/models/content.rb
+++ app/models/content.rb
@@ -1,2 +1,14 @@
 class Content < ApplicationRecord
+  has_many :faqs, dependent: :destroy
+
+  validates :body, :slug, presence: true
+  validates :slug, uniqueness: true
+
+  before_validation :set_slug, on: :create
+
+  private
+
+  def set_slug
+    self.slug ||= SecureRandom.alphanumeric(8).downcase
+  end
 end
