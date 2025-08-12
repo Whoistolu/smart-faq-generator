726836d0ed33a82dfde32d9012aafb9125ec987c
Whoistolu <digittolu25@gmail.com>
2025-08-12 18:08:42 +0100

    Add public FAQs endpoint to fetch FAQs by content slug

--- app/controllers/api/v1/public_faqs_controller.rb
+++ app/controllers/api/v1/public_faqs_controller.rb
@@ -1,2 +1,11 @@
 class Api::V1::PublicFaqsController < ApplicationController
+    class PublicFaqsController < ApplicationController
+      def show
+        content = Content.find_by!(slug: params[:slug])
+        render json: {
+          slug: content.slug,
+          faqs: content.faqs.select(:question, :answer)
+        }
+      end
+    end
 end
