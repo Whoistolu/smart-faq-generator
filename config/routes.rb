3762fdffd0616c6d6a5c5e5052a563c70917bed8
Whoistolu <digittolu25@gmail.com>
2025-08-12 18:13:48 +0100

    Refactor routes to include API namespace and versioning

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1 do
+      resources :contents, only: [:create, :show] do
+        get :faqs, on: :member
+      end
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
-
-  # Defines the root path route ("/")
-  # root "posts#index"
+      get '/faqs/public/:slug', to: 'public_faqs#show'
+    end
+  end
 end
