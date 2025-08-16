49e27d8328449dd1413217612244e00d4fe1db38
Whoistolu <digittolu25@gmail.com>
2025-08-16 23:42:45 +0100

    Refactor routes in config/routes.rb: update resources and route syntax

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :contents, only: [ :create, :show ] do
-        get :faqs, on: :member
+      resources :contents, only: [:create, :show] do
+        member do
+          get :faqs
+        end
       end
 
-      get "/faqs/public/:slug", to: "public_faqs#show"
+      get "faqs/public/:slug", to: "public_faqs#show"
     end
   end
 end
